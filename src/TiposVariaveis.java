e1351b0dc22e420ba1522f7796de4c67e2d82165
Ma1the3us <matheusrodriguesilva3@gmail.com>
2026-01-12 15:21:30 -0300

    Tipos de variaveis

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,25 @@
+public class TiposVariaveis {
+
+    // aula 1
+    /*
+     * tipos primitivos
+     * int
+     * byte
+     * short
+     * long
+     * float
+     * double
+     * boolean
+     * char
+     * 
+     * 
+     * 
+     * 
+     */
+
+   public static void main(String[] args) {
+   final double PI =3.14;
+    
+   }
+
+}
